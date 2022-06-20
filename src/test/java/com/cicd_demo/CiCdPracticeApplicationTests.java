aa555e4c56ddaaa24e58735bac0709b4ccd9edc2
Nazir <nazir.ullah@ieng-group.com>
2022-06-20 18:57:02 +0500

    v1-yml

--- src/test/java/com/cicd_demo/CiCdPracticeApplicationTests.java
+++ src/test/java/com/cicd_demo/CiCdPracticeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.cicd_demo;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,9 @@ class CiCdPracticeApplicationTests {
 
     @Test
     void contextLoads() {
+
+
+
     }
 
 }
